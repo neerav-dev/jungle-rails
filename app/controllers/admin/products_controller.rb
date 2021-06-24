9e0a8d1a9c862b555e2d276d37b345318b14441f
Neerav Patel <nickpatel.developer@gmail.com>
2021-06-23 23:01:17 -0400

    Missing Admin security fix

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'], password: ENV['HTTP_BASIC_PASSWORD']
+
+  
   def index
     @products = Product.order(id: :desc).all
   end
@@ -12,7 +15,7 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new(product_params)
 
     if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
+      redirect_to %i[:admin, :products], notice: 'Product created!'
     else
       render :new
     end
@@ -21,7 +24,7 @@ class Admin::ProductsController < ApplicationController
   def destroy
     @product = Product.find params[:id]
     @product.destroy
-    redirect_to [:admin, :products], notice: 'Product deleted!'
+    redirect_to %i[:admin, :products], notice: 'Product deleted!'
   end
 
   private
